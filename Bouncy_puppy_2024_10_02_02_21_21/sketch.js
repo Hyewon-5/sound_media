776ace67f89ffa38a1229ce5107ca1a9d3c127a1
Hyewon-5 <belle7891@naver.com>
2024-10-02 11:35:21 +0900

    Add files via upload

---
+++ Bouncy_puppy_2024_10_02_02_21_21/sketch.js
@@ -0,0 +1,17 @@
+function setup() {}
+  
+  
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  
+  arc(200,200,200,200,PI,3*PI);
+  arc(200,220,100,100,2*PI,3*PI);
+  ellipse(200,200,10,10);
+  line(150,150,170,170);
+  line(160,160,170,170);
+  ellipse(250,180,10,10);
+
+}
